ccc02fb6a33aa3e3bcf97600d5d6a447e586f4c0
unknown <vicunite2@gmail.com>
2022-09-02 15:16:15 +0530

    gmap

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-var profileImage;
-var dataObj;
 getImage();
+
+generateMap(10.305385,77.923029)
 
 
 $(document).ready(function () {
@@ -16,13 +16,15 @@ function getImage() {
     url: 'https://randomuser.me/api/',
     dataType: 'json',
     success: function (data) {
-      profileImage = data.results[0].picture.large;
-      $("#ProfileImage").attr('src', profileImage);
-      dataObj = JSON.stringify(data, undefined, 2)
-      $('#JSONString').text(`${dataObj}`);
+      $("#ProfileImage").attr('src', data.results[0].picture.large);
+      $('#JSONString').text(`${JSON.stringify(data, undefined, 2)}`);
       hideLoading();
     }
   });
+}
+
+function generateMap(latitude,longitude) {
+   $("#gooo").html(`<iframe id="googleMap" width="100%" height="100%" frameborder="0" marginheight="0" marginwidth="0" title="map" scrolling="no" src = "https://maps.google.com/maps?q=${latitude},${longitude}&hl=es;z=14&amp;output=embed"></iframe>`);
 }
 
 function showLoading() {
